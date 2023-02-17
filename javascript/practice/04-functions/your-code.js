5b1c372ad2487304b2dc3fa799858eb24f722c72
jtriolo29 <87533646+jtriolo29@users.noreply.github.com>
2023-02-17 10:54:33 -0500

    add function practice

--- javascript/practice/04-functions/your-code.js
+++ javascript/practice/04-functions/your-code.js
@@ -8,7 +8,7 @@
 // Write code to add firstNumber and secondNumber and then return the result.
 
 function addTwoNumbers(firstNumber, secondNumber) {
-    // your code goes here
+    return firstNumber + secondNumber;   
 }
 
 // Open index.html in your browser, open the JavaScript console, confirm the result.
@@ -30,8 +30,9 @@ console.log("-53 + 40 -> expected: -13, actual: %s", result);
 // You can assume text is a string with at least one character.
 // Write code to return the first character in text.
 // Hint: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String#Character_access
+
 function getFirstCharacter(text) {
-    // your code goes here.
+    return text.charAt(0);
 }
 
 console.log("getFirstCharacter =====");
@@ -48,15 +49,34 @@ console.log("'Olympus Mons' -> expected: O, actual: %s.", getFirstCharacter("Oly
 // the correct result.
 // Write your code here:
 
+function max1(num1, num2, num3) {
+  if (num1 >= num2 && num1 >= num3) {
+    return num1;
+  } else if (num2 >= num1 && num2 >= num3) {
+    return num2;
+  } else {
+    return num3;
+  }
+}
+
 // Uncomment the code below to verify your function is working.
-// console.log("max =====");
-// console.log("max(-1, 0, 1) -> expected: 1, actual: %s", max(-1, 0, 1));
-// console.log("max(11, 2, 1) -> expected: 11, actual: %s", max(11, 2, 1));
-// console.log("max(-15.67, 33.293, 0.51) -> expected: 33.293, actual: %s", max(-15.67, 33.293, 0.51));
-// console.log("max(4, 987654321, 0) -> expected: 987654321, actual: %s", max(4, 987654321, 0));
+console.log("max using if else");
+console.log("max(-1, 0, 1) -> expected: 1, actual: %s", max1(-1, 0, 1));
+console.log("max(11, 2, 1) -> expected: 11, actual: %s", max1(11, 2, 1));
+console.log("max(-15.67, 33.293, 0.51) -> expected: 33.293, actual: %s", max1(-15.67, 33.293, 0.51));
+console.log("max(4, 987654321, 0) -> expected: 987654321, actual: %s", max1(4, 987654321, 0));
 
 
-
+function max2(num1, num2, num3) {
+  return Math.max(num1, num2, num3);
+}
+  
+  // Uncomment the code below to verify your function is working.
+  console.log("max using Math.max");
+  console.log("max(-1, 0, 1) -> expected: 1, actual: %s", max2(-1, 0, 1));
+  console.log("max(11, 2, 1) -> expected: 11, actual: %s", max2(11, 2, 1));
+  console.log("max(-15.67, 33.293, 0.51) -> expected: 33.293, actual: %s", max2(-15.67, 33.293, 0.51));
+  console.log("max(4, 987654321, 0) -> expected: 987654321, actual: %s", max2(4, 987654321, 0));
 
 
 
